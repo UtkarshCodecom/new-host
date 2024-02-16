2455fe1e44ebcc87b90fb828eb61e36e9b71f033
UtkarshCodecom <86602606+UtkarshCodecom@users.noreply.github.com>
2024-02-17 00:08:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def handle_private(message: pyrogram.types.messages_and_media.message.Message, c
 			filename = "downloads/" +os.path.basename(full_path)
 			actual_path = os.path.dirname(full_path) + "\ " + os.path.basename(full_path)
 			new_string = msg.caption.replace("Nishant Jindal", "Spirexa").replace("Ananth Garg", "Spirexa")
-			bot.send_document(message.chat.id, file, thumb="thumb.png", caption=new_string, caption_entities=msg.caption_entities, reply_to_message_id=None, progress=progress, progress_args=[message,"up"])
+			bot.send_document(message.chat.id, file, thumb="thumb.png", caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=None, progress=progress, progress_args=[message,"up"])
    
 			if thumb != None: os.remove(thumb)
 			os.remove(actual_path)
